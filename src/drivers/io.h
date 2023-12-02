b3b46d2d2ccdd19a4322d2e88d46f43015177baa
vaibhav <gat.vab@gmail.com>
2023-12-02 22:37:17 +0530

    bug(fix): update io header file bugs and makefile tool path spelling. updated with clang format.

--- src/drivers/io.h
+++ src/drivers/io.h
@@ -5,12 +5,11 @@
  * This wraps the more crude register defines provided in the headers from
  * Texas instruments */
 
- // TODO: Improve multiple HW targets handling
+// TODO: Improve multiple HW targets handling
 #define LAUNCHPAD
 
-typedef enum
-{
-#if defined(LAUNCHPAD) // Launchpad (MSP430G2553IN20)
+typedef enum {
+#if defined(LAUNCHPAD)  // Launchpad (MSP430G2553IN20)
     IO_TEST_LED,
     IO_UART_RXD,
     IO_UART_TXD,
@@ -27,7 +26,7 @@ typedef enum
     IO_UNUSED_11,
     IO_UNUSED_12,
     IO_UNUSED_13,
-#elif defined(NSUMO) // Nsumo rev2 (MSP430G2553IPW28)
+#elif defined(NSUMO)  // Nsumo rev2 (MSP430G2553IPW28)
     IO_LINE_DETECT_FRONT_RIGHT,
     IO_UART_RXD,
     IO_UART_TXD,
@@ -55,42 +54,39 @@ typedef enum
 #endif
 } io_e;
 
-typedef enum
-{
+typedef enum {
     IO_SELECT_GPIO,
     IO_SELECT_ALT1,
     IO_SELECT_ALT2,
     IO_SELECT_ALT3,
-} io_select_e
+} io_select_e;
 
-typedef enum
-{
+typedef enum {
     IO_DIR_OUTPUT,
     IO_DIR_INPUT,
 } io_dir_e;
 
-typedef enum
-{
+typedef enum {
     IO_RESISTOR_DISABLED,
     IO_RESISTOR_ENABLED,
-} io_resitor_e;
+} io_resistor_e;
 
-typedef enum
-{
-    IO_OUT_LOW,  // (pull-down)
-    IO_OUT_HIGH, // (pull-up) 
+typedef enum {
+    IO_OUT_LOW,   // (pull-down)
+    IO_OUT_HIGH,  // (pull-up)
 } io_out_e;
 
-typedef enum
-{
+typedef enum {
     IO_IN_LOW,
     IO_IN_HIGH,
 } io_in_e;
 
 // TODO: structs
 
-void io_set_select(io_e io, io_select_e select);
-void io_set_direction(io_e io, io_dir_e direction);
-void io_set_resistor(io_e io, io_resitor_e resistor);
-void io_set_out(io_e io, io_out_e out);
+void    io_set_select(io_e io, io_select_e select);
+void    io_set_direction(io_e io, io_dir_e direction);
+void    io_set_resistor(io_e io, io_resistor_e resistor);
+void    io_set_out(io_e io, io_out_e out);
 io_in_e io_get_input(io_e io);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,19 +1,19 @@
 #include <msp430.h>
 
-static void test_blink_led(void)
-{
+static void test_blink_led(void) {
     // TODO: Use io functions
     P1DIR |= BIT0;
-    volatile unsigned int i; // volatile to prevent optimization
-    while(1){
-        P1NOUT ^= BIT0;
-        for (i = 1000; i>0; i--) {} //delay
+    volatile unsigned int i;  // volatile to prevent optimization
+    while (1) {
+        P1OUT ^= BIT0;
+        for (i = 1000; i > 0; i--) {
+        }  // delay
     }
 }
 
 int main(void) {
     // TODO: Move to mcu_init
-    WDTCTL = WDTPW + WDTHOLD; // stop watchdog timer
+    WDTCTL = WDTPW + WDTHOLD;  // stop watchdog timer
     test_blink_led();
     return 0;
 }
