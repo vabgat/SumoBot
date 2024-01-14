608f5e22be5f61a22db769343a6811eee27a570a
vaibhav <gat.vab@gmail.com>
2024-01-14 20:03:39 +0530

    feat(assert): assert handler and led driver
    Create an assert handler suitable for a microcontroller. Let it trigger
    a software breakpoint (if the debugger is attached) and then enter an
    endless while loop blinking the test led. The handler will later be
    extended with console tracing and turning off the motors.
    
    Also, implement a simple led driver to avoid having to interact with the
    IO functions directly, but don't use the driver in the assert handler
    to avoid it from being called recursively.
    
    Finally, wrap the __delay_cycles intrinsic in a new define macro
    BUSY_WAIT_ms that takes milliseconds (instead of cycles) as
    argument.

---
+++ src/drivers/led.h
@@ -0,0 +1,14 @@
+#ifndef LED_H
+
+// Simple driver for controlling GPIOs with LEDs connected to them
+
+typedef enum {
+    LED_TEST,
+} led_e;
+
+typedef enum { LED_STATE_OFF, LED_STATE_ON } led_state_e;
+
+void led_init();
+void led_set(led_e led, led_state_e state);
+
+#endif  // LED_H
