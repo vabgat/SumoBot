bfd72fbefcdbc6c37bc93666cb41c4ccbb292cd2
vaibhav <gat.vab@gmail.com>
2024-02-18 10:46:01 +0530

    feat(drivers): UART driver and ring buffer
    Add a driver for the UART peripheral that can transmit individual
    characters. Make it interrupt driven and put the characters in a ring
    buffer that interrupt can feed from. This is going to be used to
    implement tracing (printf) in the future.
    
    Also, write the calibrated clock values correctly.
    
    This was tested by connecting the Launchpad to a PC via USB-to-UART
    bridge use terminal.

--- src/common/assert_handler.h
+++ src/common/assert_handler.h
@@ -9,6 +9,14 @@
         }                                                                                          \
     } while (0)
 
+#define ASSERT_INTERRUPT(expression)                                                               \
+    do {                                                                                           \
+        if (!(expression)) {                                                                       \
+            while (1)                                                                              \
+                ;                                                                                  \
+        }                                                                                          \
+    } while (0)
+
 void assert_handler(void);
 
 #endif
