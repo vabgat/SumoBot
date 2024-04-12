dafbb5ff7f7c3e6cd1b7fbbe7e0ae0455ea09ada
vaibhav <gat.vab@gmail.com>
2024-04-12 17:36:58 +0530

    feat(drivers): Pulse-width modulation (PWM) with timer
    MSP430G2553 has no dedicated PWM, so use timer A0 (A1 used by IR remote)
    to emulate hardware PWM. Each timer has three capture/compare channels
    (CC) and first channel must be sacrificed for setting the base period
    (TA0CCR). The two other channels are used for one PWM output each and
    duty cycle is set by settig the timer value (TA0CCR1 and TA0CCR2). The
    CC outputs are muxed to correspoding IO pins (see io.h).
    
    Launchpad only has pins for a single motor driver, so only assign pins
    for the left motor driver. The full setup will be verified once code is
    moved to Nsumo.
    
    Also, fix a bug in io_get_current_config caused by incorrect casting.

--- src/test/test.c
+++ src/test/test.c
@@ -5,6 +5,7 @@
 #include "drivers/mcu_init.h"
 #include "drivers/uart.h"
 #include "drivers/ir_remote.h"
+#include "drivers/pwm.h"
 #include <msp430.h>
 #include "common/trace.h"
 
@@ -179,6 +180,24 @@ static void test_ir_remote(void)
     while(1){
         TRACE("Command %s", ir_remote_cmd_to_string(ir_remote_get_cmd()));
         BUSY_WAIT_ms(250);
+    }
+}
+
+SUPPRESS_UNUSED
+static void test_pwm(void)
+{
+    test_setup();
+    trace_init();
+    pwm_init();
+    const uint8_t duty_cycles[] = {100, 75, 50, 25, 1, 0};
+    const uint16_t wait_time = 3000;
+    while(1){
+        for (uint8_t i = 0; i < ARRAY_SIZE(duty_cycles); i++){
+            TRACE("Set duty cycle to %d for %d ms", duty_cycles[i], wait_time);
+            pwm_set_duty_cycle(PWM_TB6612FNG_LEFT, duty_cycles[i]);
+            pwm_set_duty_cycle(PWM_TB6612FNG_RIGHT, duty_cycles[i]);
+            BUSY_WAIT_ms(3000);
+        }
     }
 }
 
