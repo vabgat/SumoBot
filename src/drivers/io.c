0c3afe99dd30138eb1c4e0a9d091fa4feb60d06b
vaibhav <gat.vab@gmail.com>
2024-04-12 17:36:58 +0530

    feat(drivers): Pulse-width modulation (PWM) with timer
    MSP430G2553 has no dedicated PWM, so use timer A0 (A1 used by IR remote)
    to emulate hardware PWM. Each timer has three capture/compare channels
    (CC) and first channel must be sacrificed for setting the base period
    (TA0CCR). The two other channels are used for one PWM output each and
    duty cycle is set by settig the timer value (TA0CCR1 and TA0CCR2). The
    CC outputs are muxed to correspoding IO pins (see io.h).
    
    Launchpad only has pins for a single motor driver, so only assign pins
    for the left motor driver. The full setup will be verified once code is
    moved to Nsumo.
    
    Also, fix a bug in io_get_current_config caused by incorrect casting.

--- src/drivers/io.c
+++ src/drivers/io.c
@@ -85,12 +85,14 @@ static const struct io_config io_initial_config[IO_PORT_CNT * IO_PIN_CNT_PER_POR
     [IO_UART_TXD]  = {IO_SELECT_ALT3, IO_RESISTOR_DISABLED, IO_DIR_OUTPUT, IO_OUT_LOW},
     [IO_IR_REMOTE] = {IO_SELECT_GPIO, IO_RESISTOR_DISABLED, IO_DIR_INPUT, IO_OUT_LOW},
 
+    // Output driven by timer A0, direction must be set to output
+    [IO_PWM_MOTORS_LEFT] = {IO_SELECT_ALT1, IO_RESISTOR_DISABLED, IO_DIR_OUTPUT, IO_OUT_LOW},
+
 #if defined(LAUNCHPAD)
     // Unused pins
     [IO_UNUSED_1]  = UNUSED_CONFIG,
     [IO_UNUSED_2]  = UNUSED_CONFIG,
     [IO_UNUSED_3]  = UNUSED_CONFIG,
-    [IO_UNUSED_4]  = UNUSED_CONFIG,
     [IO_UNUSED_5]  = UNUSED_CONFIG,
     [IO_UNUSED_7]  = UNUSED_CONFIG,
     [IO_UNUSED_8]  = UNUSED_CONFIG,
@@ -114,7 +116,6 @@ static const struct io_config io_initial_config[IO_PORT_CNT * IO_PIN_CNT_PER_POR
     [IO_MOTORS_RIGHT_CC_2] = {IO_SELECT_GPIO, IO_RESISTOR_DISABLED, IO_DIR_OUTPUT, IO_OUT_LOW},
 
     // Output driven by timer A0, direction must be set to output
-    [IO_PWM_MOTORS_LEFT]  = {IO_SELECT_ALT1, IO_RESISTOR_DISABLED, IO_DIR_OUTPUT, IO_OUT_LOW},
     [IO_PWM_MOTORS_RIGHT] = {IO_SELECT_ALT1, IO_RESISTOR_DISABLED, IO_DIR_OUTPUT, IO_OUT_LOW},
 
     /* Input
@@ -173,14 +174,15 @@ void io_configure(io_e io, const struct io_config* config) {
 }
 
 void io_get_current_config(io_e io, struct io_config* current_config) {
-    const uint8_t port       = io_port(io);
-    const uint8_t pin        = io_pin_bit(io);
-    const uint8_t sel1       = *port_sel1_regs[port] & pin;
-    const uint8_t sel2       = *port_sel2_regs[port] & pin;
-    current_config->select   = (io_select_e)((sel2 << 1) | sel1);
-    current_config->resistor = (io_resistor_e)(*port_ren_regs[port] & pin);
-    current_config->dir      = (io_dir_e)(*port_dir_regs[port] & pin);
-    current_config->out      = (io_out_e)(*port_out_regs[port] & pin);
+    const uint8_t port     = io_port(io);
+    const uint8_t pin      = io_pin_bit(io);
+    const uint8_t sel1     = (*port_sel1_regs[port] & pin) ? 1 : 0;
+    const uint8_t sel2     = (*port_sel2_regs[port] & pin) ? 1 : 0;
+    current_config->select = (io_select_e)((sel2 << 1) | sel1);
+    current_config->resistor
+        = (*port_ren_regs[port] & pin) ? IO_RESISTOR_ENABLED : IO_RESISTOR_DISABLED;
+    current_config->dir = (*port_dir_regs[port] & pin) ? IO_DIR_OUTPUT : IO_DIR_INPUT;
+    current_config->out = (*port_out_regs[port] & pin) ? IO_OUT_HIGH : IO_OUT_LOW;
 }
 
 bool io_config_compare(const struct io_config* cfg1, const struct io_config* cfg2) {
